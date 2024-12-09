f00ca9c0639d491cba606fac315ed2238b3a1fdb
CCE107 <yoshbatula2@gmail.com>
2024-12-09 12:57:05 +0800

    yeyy

--- src/main/java/org/example/dentalmanagement/REGISTRATIONCONTROLLER/RegistrationController.java
+++ src/main/java/org/example/dentalmanagement/REGISTRATIONCONTROLLER/RegistrationController.java
@@ -1,4 +1,12 @@
 package org.example.dentalmanagement.REGISTRATIONCONTROLLER;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class RegistrationController {
+
+
+    @FXML
+    private AnchorPane containerReg;
+
 }
