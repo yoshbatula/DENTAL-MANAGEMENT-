b06f012be6968ecde3649a9753daef4d894b9b5e
CCE107 <yoshbatula2@gmail.com>
2024-12-10 09:52:11 +0800

    progress for appointments

---
+++ src/main/java/org/example/dentalmanagement/OOP/encapsulation.java
@@ -0,0 +1,10 @@
+package org.example.dentalmanagement.OOP;
+
+public class encapsulation {
+
+    private String FullName;
+    private String Gender;
+    private int Age;
+    private String Mobile;
+    private String Email;
+}
