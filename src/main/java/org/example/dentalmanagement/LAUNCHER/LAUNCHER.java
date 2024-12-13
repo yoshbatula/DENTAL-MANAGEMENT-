11ab3354ca81f8d1ed702095421ed79c037bf52b
CCE107 <yoshbatula2@gmail.com>
2024-12-13 10:43:30 +0800

    naming

--- src/main/java/org/example/dentalmanagement/LAUNCHER/LAUNCHER.java
+++ src/main/java/org/example/dentalmanagement/LAUNCHER/LAUNCHER.java
@@ -12,7 +12,7 @@ public class LAUNCHER extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/dentalmanagement/ADMIN INTERFACE/MainStructureAdmin.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("MAIN!");
         stage.setScene(scene);
         stage.show();
     }
