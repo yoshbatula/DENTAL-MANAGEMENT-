b241cccec5d2da2d9bea0cbf4a0252a7a65e038e
CCE107 <yoshbatula2@gmail.com>
2024-12-13 14:19:30 +0800

    finally the combo box for doctors and services

--- src/main/java/org/example/dentalmanagement/ADMINCONTROLLER/AppointmentController.java
+++ src/main/java/org/example/dentalmanagement/ADMINCONTROLLER/AppointmentController.java
@@ -1,19 +1,35 @@
 package org.example.dentalmanagement.ADMINCONTROLLER;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.example.dentalmanagement.DATABASE.DATABASECONNECTIVITY;
+
+import java.net.URL;
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import static java.sql.Date.valueOf;
 
-public class AppointmentController {
+public class AppointmentController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        AppointmentTime.setItems(Time);
+        Doctors();
+        Services();
+    }
 
     @FXML
     private TextField AddressTF;
@@ -34,7 +50,25 @@ public class AppointmentController {
     private Button ProceedBTN;
 
     @FXML
+    private ComboBox<String> ServicesComboBox;
+
+    @FXML
+    private DatePicker AppointmentDatePicekr;
+
+    @FXML
+    private ComboBox<String> AppointmentTime;
+
+    @FXML
+    private ComboBox<String> DoctorComboBox;
+
+    @FXML
     private Label PatientIDLabel;
+
+    private List<String> DoctorNames;
+
+    private List<String> ServicesNames;
+
+    private ObservableList<String> Time = FXCollections.observableArrayList("9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm", "6:00pm");
 
     public void Patient() {
         DATABASECONNECTIVITY db = new DATABASECONNECTIVITY();
@@ -99,6 +133,47 @@ public class AppointmentController {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void Doctors() {
+        this.DoctorNames = new ArrayList<>();
+
+        DATABASECONNECTIVITY db = new DATABASECONNECTIVITY();
+
+        try {
+            Connection conn = db.getConnection();
+            String query = "SELECT FullName FROM doctor";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+
+                this.DoctorNames.add(rs.getString("FullName"));
+            }
+            this.DoctorComboBox.getItems().addAll(this.DoctorNames);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Services() {
+        DATABASECONNECTIVITY db = new DATABASECONNECTIVITY();
+        this.ServicesNames = new ArrayList<>();
+
+        try {
+            Connection conn = db.getConnection();
+            String query = "SELECT ServiceName FROM Services";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                this.ServicesNames.add(rs.getString("ServiceName"));
+            }
+
+            this.ServicesComboBox.getItems().addAll(this.ServicesNames);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
